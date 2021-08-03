62be7fffaeda1e241105af9b552c9fa699924e19
Oscar Solanilla <odsolanillaa@ut.edu.co>
2021-08-03 15:37:33 -0500

    prueba de error desde master con nuevos botones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel RAMA </title>
+        <title>Laravel RAMA MASTER PARA LAS PRUEBAS PRINCIPALES</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -25,9 +25,11 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-14 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">CASAS</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">CONDOMINIO</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">PRUEBA2 DESDE MASTER</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">PRUEBA3 DESDE MASTER</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">INICIO DE LAS CASAS</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">INICIO DE LOS EDIFICIOS</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
